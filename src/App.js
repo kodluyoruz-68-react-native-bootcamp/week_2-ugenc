7347901e07c7c9ba6914242a73255b216bec7df5
ugur <ugur.genc33@gmail.com>
2020-12-19 01:58:21 +0300

    All features are implemented

--- src/App.js
+++ src/App.js
@@ -1,17 +1,114 @@
-import React from 'react';
-import {SafeAreaView, View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  KeyboardAvoidingView,
+  FlatList,
+} from 'react-native';
+import NewNoteEntry from './components/NewNoteEntry';
+import NoteItem from './components/NoteItem';
 
 /**
  * TextInput: testID="input" (component which is user types the todo text)
  * TouchableOpacity: testID="button" (component which is saves the todo to list)
  * FlatList: testID="list" (list of todo)
  */
+const notes = [
+  {
+    id: '0',
+    title: 'İki Ekmek Al',
+    isDone: false,
+  },
+  {
+    id: '1',
+    title: 'React Native ödevini yap',
+    isDone: true,
+  },
+  {
+    id: '3',
+    title: 'JS çalış',
+    isDone: true,
+  },
+];
 
 function App() {
+  const [todos, setTodos] = useState(notes);
+
+  function addItem(newText) {
+    const newItem = {
+      id: Math.floor(Math.random() * 10000).toString(),
+      title: newText,
+      isDone: false,
+    };
+    setTodos([newItem, ...todos]);
+  }
+
+  const changeDoneState = (id) => {
+    var updatedTodos = todos.map((todo) =>
+      todo.id === id
+        ? {
+            ...todo,
+            isDone: !todo.isDone,
+          }
+        : todo,
+    );
+
+    setTodos(updatedTodos);
+  };
+
+  const deleteItem = (id) => {
+    const updatedTodoList = todos.filter((item) => item.id !== id);
+
+    setTodos(updatedTodoList);
+  };
+
+  function renderNote(note) {
+    return (
+      <NoteItem
+        item={note}
+        handleLongPress={deleteItem}
+        handlePress={changeDoneState}
+      />
+    );
+  }
+
   return (
-    // ...
-    null
+    <SafeAreaView style={styles.mainContainer}>
+      <KeyboardAvoidingView
+        style={styles.mainContainer}
+        behavior="padding"
+        enabled>
+        <View style={styles.notesContainer}>
+          <FlatList
+            data={todos}
+            keyExtractor={(item) => item.id}
+            renderItem={renderNote}
+          />
+        </View>
+        <View style={styles.newNotePanel}>
+          <NewNoteEntry handleSubmit={addItem} />
+        </View>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 }
 
 export default App;
+
+const styles = StyleSheet.create({
+  mainContainer: {flex: 1, backgroundColor: '#f0f0f0f0'},
+  notesContainer: {
+    flex: 1,
+  },
+  newNotePanel: {
+    flex: 0,
+    height: 60,
+
+    position: 'relative',
+    marginHorizontal: 20,
+    bottom: 30,
+  },
+});
---
+++ src/components/NewNoteEntry.js
@@ -0,0 +1,74 @@
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+
+export default function NewNoteEntry({handleSubmit}) {
+  const [newItem, setNewItem] = useState([]);
+  const [inputText, setInputText] = useState('');
+
+  return (
+    <View style={styles.noteContainer}>
+      <TextInput
+        placeholder="pour your mind here"
+        onChangeText={(text) => setInputText(text)}
+        value={inputText}
+        style={styles.textInput}
+      />
+      <TouchableOpacity
+        style={styles.buttonContainer}
+        onPress={() => {
+          handleSubmit(inputText);
+          setInputText('');
+        }}>
+        <Text style={styles.buttonText}>+</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  noteContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    borderRadius: 20,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 8,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 11.14,
+
+    elevation: 17,
+  },
+  textInput: {
+    flex: 0.9,
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingVertical: 20,
+    borderRadius: 20,
+    backgroundColor: 'white',
+    fontSize: 20,
+  },
+  buttonContainer: {
+    flex: 0.15,
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'center',
+    height: 60,
+    backgroundColor: 'orange',
+    borderTopRightRadius: 20,
+    borderBottomRightRadius: 20,
+  },
+  buttonText: {
+    fontSize: 40,
+    fontWeight: '700',
+    color: 'white',
+  },
+});
